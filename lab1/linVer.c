7c7f2d4db615521bda93d9e3084d402eba644bc0
Pavel V <mental.notes@yandex.ru>
2018-09-09 21:29:53 +0300

    Отредактировал получение сист. инфы для Linux

--- lab1/linVer.c
+++ lab1/linVer.c
@@ -1,29 +1,7 @@
-#include <stdio.h>
-#include <fcntl.h>
-
-void viewfile (int fd, ssize_t ret, char ch )
+#include <stdlib.h>
+ 
+int main(void)
 {
-    while ((ret = read (fd, &ch, 1)) > 0)
-    {
-        putchar (ch);
-    }
-    close (fd);
-}
-
-int main ()
-{
-    int fd;
-    ssize_t ret;
-    char ch;
-
-    printf("\n\n");
-    fd = open ("/etc/release", O_RDONLY);
-    viewfile(fd, ret, ch); 
-    fd = open ("/etc/issue", O_RDONLY);
-    printf("Название системы и версия ядра\n");
-    viewfile(fd, ret, ch);   
-    fd = open ("/etc/motd", O_RDONLY); 
-    viewfile(fd, ret, ch);
-    fd = open ("/etc/lsb-release", O_RDONLY);
-    viewfile(fd, ret, ch);
+    system("uname -a");
+    return 0;
 }
